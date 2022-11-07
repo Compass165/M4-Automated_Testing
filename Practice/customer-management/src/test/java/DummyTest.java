1fb5ab8ae4138bdbaef5d14253e4394f70fc946f
Compass <ndphong165@gmail.com>
2022-11-07 14:13:52 +0700

    van bug cho chi k.a fix

---
+++ Practice/customer-management/src/test/java/DummyTest.java
@@ -0,0 +1,20 @@
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//public class DummyTest {
+//    @Test
+//    void assertionWorked() {
+//        int actual = 1+1;
+//        int expected = 2;
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    void assertionWorked2() {
+//        int actual = 1-1;
+//        int expected = 2;
+//        assertEquals(expected, actual);
+//    }
+//
+//}
